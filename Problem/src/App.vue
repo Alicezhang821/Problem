452b30b7364173531ff7bf28e00ed20ef4625486
Michael Mano <michael.mano26@gmail.com>
2019-11-28 15:39:26 +1000

    Update issue from stackoverflow

--- Problem/src/App.vue
+++ Problem/src/App.vue
@@ -1,21 +1,6 @@
 <template>
   <div id="app">
-  <table class="table table-striped mt4">
-    <thead>
-      <tr>
-        <th scope="col">Id</th>
-        <th scope="col">Title</th>
-        <th scope="col">Body</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="post in posts" :key="post.id">
-        <td>{{post.id}}</td>
-        <td>{{post.title}}</td>
-        <td>{{post.body}}</td>
-      </tr>
-    </tbody>
-  </table>
+    <PostExample />
   </div>
 </template>
 
--- Problem/src/components/Posts.vue
+++ Problem/src/components/Posts.vue
@@ -1,4 +1,21 @@
-
+<template>
+  <table class="table table-striped mt4">
+    <thead>
+      <tr>
+        <th scope="col">Id</th>
+        <th scope="col">Title</th>
+        <th scope="col">Body</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="post in posts" :key="post.id">
+        <td>{{post.id}}</td>
+        <td>{{post.title}}</td>
+        <td>{{post.body}}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
 <script>
 export default{
   name: 'PostsExample',
