2e50e78476cf4f907384c169908f6bd031d789d4
alicezhang821 <alicezhang821@gmail.com>
2019-11-28 11:25:33 +1100

    222

---
+++ Problem/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+  <table class="table table-striped mt4">
+    <thead>
+      <tr>
+        <th scope="col">Id</th>
+        <th scope="col">Title</th>
+        <th scope="col">Body</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="post in posts" :key="post.id">
+        <td>{{post.id}}</td>
+        <td>{{post.title}}</td>
+        <td>{{post.body}}</td>
+      </tr>
+    </tbody>
+  </table>
+  </div>
+</template>
+
+<script>
+import PostsExample from './components/Posts'
+
+export default {
+  name: 'App',
+  components: {
+    PostsExample
+  }
+}
+</script>
+
+<style>
+@import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+#app {
+
+}
+</style>
---
+++ Problem/src/components/Posts.vue
@@ -0,0 +1,16 @@
+
+<script>
+export default{
+  name: 'PostsExample',
+  data () {
+    return {
+      posts: [
+        {id: 1, title: 'Post 1', body: 'kasfjk'},
+        {id: 2, title: 'Post 2', body: 'kasfjk'},
+        {id: 3, title: 'Post 3', body: 'kasfjk'}
+      ]
+
+    }
+  }
+}
+</script>
